69c20500342032c9acd3c2a889909e8ebedf1421
cstenico <k.stenico@gmail.com>
2018-07-03 21:42:03 -0300

    added tests to locust

--- locustsus.py
+++ locustsus.py
@@ -1,19 +1,29 @@
 from locust import HttpLocust, TaskSet, task
 
 class WebsiteTasks(TaskSet):
+
+    @task
+    def login(self):
+        self.client.post("/", {"email":"stenico.camila@gmail.com", "password":"1234"})
         
     @task
     def list_storages(self):
         self.client.get("/local_storages")
-        print "geting local storages"
 
     @task
     def list_medicines(self):
         self.client.get("/medicines")
-        print "geting medicines"
+    
+    @task
+    def list_stocks(self):
+        self.client.get("/stocks")
+
+    @task
+    def new_patient(self):
+        self.client.post("/patients/new", {"name":"camila", "cpf":"44757742843", "telefone":"19988094195"})
 
 
 class WebsiteUser(HttpLocust):
     task_set = WebsiteTasks
-    min_wait = 5000
-    max_wait = 15000
+    min_wait = 20000
+    max_wait = 50000
