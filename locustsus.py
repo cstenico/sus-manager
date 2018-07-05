2fdbaaebce707c6f9b6aed67b351790dbb037ce2
g6cn18 g6cn18 <k.stenico@gmail.com>
2018-07-05 00:25:27 -0300

    cluster commit

--- locustsus.py
+++ locustsus.py
@@ -1,11 +1,10 @@
 from locust import HttpLocust, TaskSet, task
+import resource
+
+resource.setrlimit(resource.RLIMIT_NOFILE, (999999, 999999))
 
 class WebsiteTasks(TaskSet):
 
-    @task
-    def login(self):
-        self.client.post("/", {"email":"stenico.camila@gmail.com", "password":"1234"})
-        
     @task
     def list_storages(self):
         self.client.get("/local_storages")
@@ -13,17 +12,8 @@ class WebsiteTasks(TaskSet):
     @task
     def list_medicines(self):
         self.client.get("/medicines")
-    
-    @task
-    def list_stocks(self):
-        self.client.get("/stocks")
-
-    @task
-    def new_patient(self):
-        self.client.post("/patients/new", {"name":"camila", "cpf":"44757742843", "telefone":"19988094195"})
-
 
 class WebsiteUser(HttpLocust):
     task_set = WebsiteTasks
-    min_wait = 20000
-    max_wait = 50000
+    min_wait = 20
+    max_wait = 5000000
